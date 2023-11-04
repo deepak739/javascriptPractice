feef0d98c0050020caf2e40fc295eeffb069a97e
deepak739 <46920411+deepak739@users.noreply.github.com>
2023-11-04 15:13:51 +0000

    allAtOnce

---
+++ 03_basics/iife.js
@@ -0,0 +1,12 @@
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh');
+
+((testName) => {
+    console.log("Test Database Connection ");
+})()
